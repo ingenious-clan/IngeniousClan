d43c87dac82d19cfe3ec0abaa49537c561b4a301
yokesh_ks <yokesh.ks@increscotech.com>
2024-02-03 15:09:35 +0530

    first commit

---
+++ src/utils/data.ts
@@ -0,0 +1,29 @@
+const services = [
+  {
+    title: "FontWise",
+    description: "Responsive Font Calculator for Modern Web Design",
+    icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
+        </svg>`,
+  },
+  {
+    title: "Code Glow",
+    description:
+      "Transform Code into an Art, Engage Your Users with Brilliance.",
+    icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
+        </svg>`,
+  },
+  {
+    title: "README Magic",
+    description: "Effortless Documentation, Every Detail Perfectly Captured.",
+    icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
+        </svg>`,
+  },
+  {
+    title: "JSON Wizard",
+    description: "Simplify JSON Management, Unleash the Beauty in Your Data.",
+    icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
+        </svg>`,
+  },
+];
+
+export { services };
